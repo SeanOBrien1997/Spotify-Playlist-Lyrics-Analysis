33b2f765465bdc5d942a8eef94b4dca74e5934f7
Sean O Brien <sean.obrien.2016@mumail.ie>
2021-11-15 05:18:16 +0000

    Add communication between Lambda and node services.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,28 @@
 import express from 'express';
+import axios from 'axios'
 
+const LAMBDA_BASE_URL: string = 'http://lambda-nltk'
+const LAMBDA_PORT: number = 8080
+const LAMBDA_ENDPOINT: string = '2015-03-31/functions/function/invocations'
+const LAMBDA_URL: string = `${LAMBDA_BASE_URL}:${LAMBDA_PORT}/${LAMBDA_ENDPOINT}`
 const app = express();
 const port: Number = 5001;
 
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    const data: string = 'Hello world from node';
+    lambdaRequest(data);
 });
+
+const lambdaRequest = (msg: string) => {
+    console.log('Sending request to: ' + LAMBDA_URL);
+    axios.post(LAMBDA_URL, {
+        message: msg
+    }).then(res => {
+        console.log(res.status);
+        console.log(res);
+    }).catch(error => {
+        console.error(error);
+    })
+}
